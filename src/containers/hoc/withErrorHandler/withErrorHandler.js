179df7407ca63ecd0a116e0b92b0da9981bcf338
aijazbinqasim <aijazbinqasim@hotmail.com>
2021-10-24 15:10:54 +0500

    minor changes

--- src/containers/hoc/withErrorHandler/withErrorHandler.js
+++ src/containers/hoc/withErrorHandler/withErrorHandler.js
@@ -36,7 +36,7 @@ const withErrorHandler = (WrappedComponent, axios) => {
 
                     <WrappedComponent {...this.props} />
                 </>
-            );
+            )
         }
     }
 }
